b8fffab9d054e122791797eaa925db0ffc3e3a43
Harri Rautila <harri.rautila@gmail.com>
2018-12-09 00:22:37 +0200

    Updated.

--- print.c
+++ print.c
@@ -43,7 +43,16 @@ void cvx_mat_printf(FILE *f, const char *format, cvx_matrix_t *g, const char *s)
     const char *frm = format ? format : "%9.2e";
     if (s)
         fprintf(f, "%s\n", s);
-    cvxm_printf(f, frm, g);
+    for (int i = 0; i < g->rows; i++) {
+        fprintf(f, "[");
+        for (int j = 0; j < g->cols; j++) {
+            if (j > 0)
+                fprintf(f, ",");
+            fprintf(f, frm, cvxm_get(g, i, j));
+        }
+        fprintf(f, "]\n");
+    }
+    //cvxm_printf(f, frm, g);
 }
 
 void cvx_scaling_printf(FILE *f, const char *format, cvx_scaling_t *W, const char *s)
@@ -52,11 +61,18 @@ void cvx_scaling_printf(FILE *f, const char *format, cvx_scaling_t *W, const cha
     char buf[64];
     fprintf(f, "** start scaling %s\n", s);
 
-    cvx_scaling_elem(&m, W, CVXWS_D, 0);
-    cvx_mat_printf(f, format, &m, "D");
-    cvx_scaling_elem(&m, W, CVXWS_DI, 0);
-    cvx_mat_printf(f, format, &m, "DI");
-
+    if (W->dnlsz > 0) {
+        cvx_scaling_elem(&m, W, CVXWS_DNL, 0);
+        cvx_mat_printf(f, format, &m, "DNL");
+        cvx_scaling_elem(&m, W, CVXWS_DNLI, 0);
+        cvx_mat_printf(f, format, &m, "DNLI");
+    }
+    if (W->dsz > 0) {
+        cvx_scaling_elem(&m, W, CVXWS_D, 0);
+        cvx_mat_printf(f, format, &m, "D");
+        cvx_scaling_elem(&m, W, CVXWS_DI, 0);
+        cvx_mat_printf(f, format, &m, "DI");
+    }
     if (W->vcount > 0) {
         cvx_scaling_elem(&m, W, CVXWS_BETA, 0);   
         cvx_mat_printf(f, format, &m, "beta");
@@ -69,6 +85,13 @@ void cvx_scaling_printf(FILE *f, const char *format, cvx_scaling_t *W, const cha
 
     if (W->rcount > 0) {
         for (int i = 0; i < W->rcount; i++) {
+            cvx_scaling_elem(&m, W, CVXWS_R, i);   
+            sprintf(buf, "R[%d]", i);
+            cvx_mat_printf(f, format, &m, buf);
+
+            cvx_scaling_elem(&m, W, CVXWS_RTI, i);   
+            sprintf(buf, "RTI[%d]", i);
+            cvx_mat_printf(f, format, &m, buf);
         }
     }
     fprintf(f, "** end scaling %s\n", s);
