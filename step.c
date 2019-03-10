c8ee01019c8a02bf7b7f4b081ac8ac5a2a5ea920
Harri Rautila <harri.rautila@gmail.com>
2019-03-10 21:25:28 +0200

    Support for convex target function.

--- step.c
+++ step.c
@@ -15,11 +15,11 @@
  *    Workspace
  *
  * Computes min {t | x + t*e >= 0}, where e is defined as follows
- *    
+ *
  *  - For the nonlinear and 'L' blocks: e is the vector of ones.
  *  - For the 'Q' blocks: e is the first unit vector.
  *  - For the 'S' blocks: e is the identity matrix.
- *    
+ *
  */
 cvx_float_t cvx_max_step(cvx_matgrp_t *x_g,
                          cvx_matgrp_t *sigma_g,
@@ -29,10 +29,19 @@ cvx_float_t cvx_max_step(cvx_matgrp_t *x_g,
     cvx_matrix_t u, u1;
     cvx_index_t *index = x_g->index;
     cvx_size_t m;
-    
+    // static int svd = 0;
+
+    if (index->indnlt) {
+        m = cvx_mgrp_elem(&u, x_g, CVXDIM_NLTARGET, 0);
+        for (cvx_size_t k = 0; k < m; k++) {
+            if (tmax < -cvxm_get(&u, k, 0)) {
+                tmax = -cvxm_get(&u, k, 0);
+            }
+        }
+    }
     if (index->indnl) {
         m = cvx_mgrp_elem(&u, x_g, CVXDIM_NONLINEAR, 0);
-        for (int k = 0; k < m; k++) {
+        for (cvx_size_t k = 0; k < m; k++) {
             if (tmax < -cvxm_get(&u, k, 0)) {
                 tmax = -cvxm_get(&u, k, 0);
             }
@@ -40,7 +49,7 @@ cvx_float_t cvx_max_step(cvx_matgrp_t *x_g,
     }
     if (index->indl) {
         m = cvx_mgrp_elem(&u, x_g, CVXDIM_LINEAR, 0);
-        for (int k = 0; k < m; k++) {
+        for (cvx_size_t k = 0; k < m; k++) {
             if (tmax < -cvxm_get(&u, k, 0)) {
                 tmax = -cvxm_get(&u, k, 0);
             }
@@ -62,22 +71,38 @@ cvx_float_t cvx_max_step(cvx_matgrp_t *x_g,
         cvx_memblk_t mem;
         for (int k = 0; k < cvx_mgrp_count(x_g, CVXDIM_SDP); k++) {
             m = cvx_mgrp_elem(&u, x_g,  CVXDIM_SDP, k);
+#if 0
+            if (svd > 3 && svd < 7) {
+                cvx_mat_printf(stderr, "%.12e", &u, "svd.u");
+            }
+#endif
             if (sigma_g) {
                 cvx_mgrp_elem(&lk, sigma_g, CVXDIM_SDP, k);
                 cvxm_evd_sym(&lk, &u, CVX_WANTV|CVX_LOWER|ARMAS_FORWARD, wrk);
                 v = - cvxm_get(&lk, 0, 0);
+#if 0
+                if (svd > 3 && svd < 7) {
+                    cvx_mat_printf(stderr, "%.12e", &lk, "svd.lk");
+                }
+#endif
             } else {
                 cvxm_map_data(&Q, m, m, __mblk_offset(wrk, 0));
                 cvxm_map_data(&w, m, 1, __mblk_offset(wrk, m*m));
                 __mblk_subblk(&mem, wrk, m*m +m);
                 cvxm_copy(&Q, &u, 0);
                 cvxm_evd_sym(&w, &Q, CVX_LOWER, &mem);
-                
+
                 v = - cvxm_get(&w, 0, 0);
+#if 0
+                if (svd > 3 && svd < 7) {
+                    cvx_mat_printf(stderr, "%.12e", &w, "svd.w");
+                }
+#endif
             }
             if (tmax < v)
                 tmax = v;
         }
+        //svd++;
     }
     return tmax;
 }
