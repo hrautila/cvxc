03bd00040da89d63d82dc0938c39b36b1d33dd12
Harri Rautila <harri.rautila@gmail.com>
2019-03-10 21:26:25 +0200

    Use cvx_size_t instead of int.

--- kktldl.c
+++ kktldl.c
@@ -1,5 +1,5 @@
 
-// Copyright: Harri Rautila, 2016 <harri.rautila@gmail.com>
+// Copyright: Harri Rautila, 2018 <harri.rautila@gmail.com>
 
 #include "convex.h"
 
@@ -57,19 +57,19 @@ static cvx_kktfuncs_t ldlfunctions = {
 
 // Solution of KKT equations by a dense LDL factorization of the 
 // 3 x 3 system.
-//    
+//
 // Returns a solver that (1) can computes the LDL factorization of
-//    
-// [ H           A'   GG'*W^{-1} ] 
+//
+// [ H           A'   GG'*W^{-1} ]
 // [ A           0    0          ],
-// [ W^{-T}*GG   0   -I          ] 
-//    
-// given H, Df, W, where GG' = [Df; G], and (2) compute solution for 
-//    
+// [ W^{-T}*GG   0   -I          ]
+//
+// given H, Df, W, where GG' = [Df; G], and (2) compute solution for
+//
 //  [ H     A'   GG'   ]   [ ux ]   [ bx ]
 //  [ A     0    0     ] * [ uy ] = [ by ].
 //  [ GG    0   -W'*W  ]   [ uz ]   [ bz ]
-//    
+//
 // H is n x n,  A is p x n, Df is mnl x n, G is N x n where
 // N = dims['l'] + sum(dims['q']) + sum( k**2 for k in dims['s'] ).
 //
@@ -86,7 +86,7 @@ int ldl_factor(cvx_kktsolver_t *S,
     cvx_size_t rG, cG;
     cvx_matrix_t Kt, colvec, g0;
     cvx_matgrp_t col_g;
-    
+
     cvxm_size(&rG, &cG, cp->G);
 
     ldl->W = W;
@@ -100,9 +100,10 @@ int ldl_factor(cvx_kktsolver_t *S,
     // copy A to K
     cvxm_view_map(&Kt, &ldl->K, ldl->n, 0, ldl->p, ldl->n);
     cvxm_copy(&Kt, ldl->A, 0);
-    
+
+    //cvx_mat_printf(stdout, "%4.1f", Df, "Df");
     // copy scaled [Df; G].T to K ; column by column
-    for (int k = 0; k < ldl->n; k++) {
+    for (cvx_size_t k = 0; k < ldl->n; k++) {
         // g is (mnl + G.Rows(), 1) matrix, Df is (mnl, n), G is (N, n)
         if (ldl->mnl > 0) {
             // copy Df column; Df is mnl-by-n matrix
@@ -115,21 +116,23 @@ int ldl_factor(cvx_kktsolver_t *S,
         cvxm_view_map(&g0, &ldl->g, ldl->mnl, 0, rG, 1);
         cvxm_copy(&g0, &colvec, 0);
         // make it matrix group; like group on z vector
-        cvx_mgrp_init(&col_g, &g0, cp->index_g);
+        cvx_mgrp_init(&col_g, &ldl->g, cp->index_g);
         // scale column; with W^-T
         cvx_scale(&col_g, W, CVX_INV|CVX_TRANS, &cp->work);
         // copy to K in packed storage;
         cvxm_view_map(&colvec, &ldl->K, ldl->n+ldl->p, k, ldl->ldK-ldl->p-ldl->n, 1);
-        cvx_pack(&colvec, &g0, col_g.index);
+        cvx_pack(&colvec, col_g.mat, col_g.index);
     }
 
     // set trailing diagonal to -1
-    for (int k = ldl->n + ldl->p; k < ldl->ldK; k++) {
+    for (cvx_size_t k = ldl->n + ldl->p; k < ldl->ldK; k++) {
         cvxm_set(&ldl->K, k, k, -1.0);
     }
-    //cvx_mat_printf(stdout, "%13.6e", &ldl->K, "K");
+    //cvx_mat_printf(stdout, "%4.1f", &ldl->K, "K");
     int err = cvxm_ldlfactor(&ldl->K, ldl->ipiv, CVX_LOWER, &ldl->work);
-    //cvx_mat_printf(stdout, "%13.6e", &ldl->K, "factored(K)");
+    //cvx_mat_printf(stdout, "%4.1f", &ldl->K, "factored(K)");
+    //cvx_mat_test_nan("LDLKKT_POSTFACTOR_K", &ldl->K);
+
     return err;
 }
 
@@ -168,7 +171,7 @@ int ldl_solve(cvx_kktsolver_t *S,
     cvx_pack(&u0, z_g->mat, z_g->index);
 
     err = cvxm_ldlsolve(&ldl->u, &ldl->K, ldl->ipiv, CVX_LOWER, &ldl->work);
-    
+
     cvxm_view_map(&u0, &ldl->u, 0, 0, ldl->n, 1);
     cvxm_copy(x, &u0, CVX_ALL);
 
@@ -190,27 +193,30 @@ int ldl_init(cvx_kktsolver_t *S,
 {
     cvx_ldlsolver_t *ldl = (cvx_ldlsolver_t *)S;
     ldl->fnc = &ldlfunctions;
-    
+
     ldl->cp = cp;
     ldl->ldK = n + m;                                   // space for H and A (H can be zeroes)
-    ldl->ldK += cvx_dimset_sum(dims, CVXDIM_NONLINEAR);// space for Df (can be zero)    
-    ldl->ldK += cvx_dimset_sum(dims, CVXDIM_LINEAR);           // rest is space for G
+    ldl->ldK += cvx_dimset_sum(dims, CVXDIM_NONLINEAR); // space for Df (can be zero)
+    ldl->ldK += cvx_dimset_sum(dims, CVXDIM_LINEAR);    // rest is space for G
     ldl->ldK += cvx_dimset_sum(dims, CVXDIM_SOCP);
     ldl->ldK += cvx_dimset_sum_packed(dims, CVXDIM_SDP);
 
     cvxm_init(&ldl->K, ldl->ldK, ldl->ldK);
     ldl->ipiv = (int *)calloc(ldl->ldK, sizeof(int));
-    
+
     cvxm_init(&ldl->u, ldl->ldK, 1);
     cvxm_init(&ldl->g, ldl->ldK, 1);
 
     // compute size for workspace
+#if 0
     armas_wbuf_t wb = ARMAS_WBNULL;
     armas_pivot_t P;
-    
+
     armas_pivot_make(&P, ldl->ldK, ldl->ipiv);
     armas_d_bkfactor_w(&ldl->K, &P, CVX_LOWER, &wb, (armas_conf_t *)0);
-    __mblk_init(&ldl->work, 4*wb.bytes);
+#endif
+    cvx_size_t wbytes = cvxm_ldlwork(&ldl->K);
+    __mblk_init(&ldl->work, 4*wbytes);
 
     ldl->dims = dims;
     ldl->A = cp->A;
@@ -263,7 +269,7 @@ void ldl_free(cvx_kktsolver_t *kkt)
 {
     if (!kkt)
         return;
-    
+
     cvx_ldlsolver_t *ldl = (cvx_ldlsolver_t *)kkt;
     cvxm_release(&ldl->K);
     cvxm_release(&ldl->u);
