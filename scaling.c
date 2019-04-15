851e89a411e276c5f41b565c611899d7e25e566c
Harri Rautila <harri.rautila@gmail.com>
2019-04-15 23:54:10 +0300

    Remove reference to dimension set.

--- scaling.c
+++ scaling.c
@@ -22,7 +22,8 @@ cvx_size_t cvx_scaling_bytes(cvx_size_t *isize, const cvx_dimset_t *dims)
 
     // calculate index space length in bytes and align to 64bits;
     // (3 is for linear, non-linear and non-linear target spaces.)
-    cvx_size_t itotal = (3 + dims->qlen + 2*dims->slen)*sizeof(cvx_size_t);
+    // SDP saved as tuple (offset, size)
+    cvx_size_t itotal = (3 + dims->qlen + 1 + 4*dims->slen)*sizeof(cvx_size_t);
     itotal += (itotal & 0x7) != 0 ? 8 - (itotal & 0x7) : 0;
 
     // matrix data space length
@@ -79,15 +80,14 @@ cvx_size_t cvx_scaling_make(cvx_scaling_t *W, const cvx_dimset_t *dims, void *me
             W->indv = W->indexes;
         }
         if (dims->slen > 0) {
-            W->indr = &W->indexes[dims->qlen];
-            W->indrti = &W->indexes[dims->qlen + dims->slen];
+            W->indr = &W->indexes[dims->qlen == 0 ? 0 : dims->qlen+1];
+            W->indrti = &W->indexes[(dims->qlen == 0 ? 0 : dims->qlen+1) + 2*dims->slen];
         }
     } else {
         W->indexes = W->indv = W->indr = W->indrti = (cvx_size_t *)0;
         W->vcount = W->rcount = 0;
     }
 
-    W->dims = dims;
     // setup indexes
     cvx_size_t offset = 0;
     W->dnltsz = dims->iscpt ? 1 : 0;
@@ -100,7 +100,7 @@ cvx_size_t cvx_scaling_make(cvx_scaling_t *W, const cvx_dimset_t *dims, void *me
     // Arrange NLTARGET, NONLINEAR and LINEAR spaces to continous
     // memory blocks for direct and inverse cases.
     // Memory layout:
-    //   |DNLT|DNL|D|DNLTI|DNLI|DI|BETA|V[0]| ..|V[q]|R[0]|RTI[0]|..|R[n]|RTI[n]|
+    //   |DNLT|DNL|D|DNLTI|DNLI|DI|BETA|V[0]| ..|V[q]|R[0]|..|R[n]|RTI[0]|..|RTI[n]|
     //
     if (dims->iscpt > 0) {
         // Direct pointers to DNLT data space
@@ -139,17 +139,23 @@ cvx_size_t cvx_scaling_make(cvx_scaling_t *W, const cvx_dimset_t *dims, void *me
         W->beta = &W->data[offset];
         offset += dims->qlen;
         // Sizes of and offsets to V vector data space
-        for (int k = 0; k < dims->qlen; k++) {
+        for (cvx_size_t k = 0; k < dims->qlen; k++) {
             W->indv[k] = offset;
             offset += dims->qdims[k];
         }
+        // offset past last entry to allow length computation
+        W->indv[dims->qlen] = offset;
     }
     if (dims->slen > 0) {
         // Save sizes of and offsets to R/RTI matrices data space
-        for (int k = 0; k < dims->slen; k++) {
+        for (cvx_size_t k = 0; k < 2*dims->slen; k += 2) {
             W->indr[k] = offset;
+            W->indr[k+1] = dims->sdims[k];
             offset += dims->sdims[k] * dims->sdims[k];
+        }
+        for (cvx_size_t k = 0; k < 2*dims->slen; k += 2) {
             W->indrti[k] = offset;
+            W->indrti[k+1] = dims->sdims[k];
             offset += dims->sdims[k] * dims->sdims[k];
         }
     }
@@ -159,12 +165,6 @@ cvx_size_t cvx_scaling_make(cvx_scaling_t *W, const cvx_dimset_t *dims, void *me
 /*
  *  Scaling matrix W, one big allocation block that is divided to index space
  *  and matrix data space.
- *
- *  Size of index space is:
- *       (N_SOCP + 2*N_SDP)*sizeof(size_t) aligned to 64bit
- *  Size of matrix data space:
- *       (2*(NONLINEAR + LINEAR) + sum(SOCP_i) + N_SOCP + 2*sum(SDP_i^2)) * sizeof(cvx_float_t)
- *
  */
 
 /**
@@ -190,8 +190,8 @@ cvx_scaling_t *cvx_scaling_init(cvx_scaling_t *W, const cvx_dimset_t *dims)
 /**
  * @brief Create a new scaling matrix for dimension set.
  *
- * @return 
- *    Pointer to 
+ * @return
+ *    Pointer to
  */
 cvx_scaling_t *cvx_scaling_new(const cvx_dimset_t *dims)
 {
@@ -290,18 +290,21 @@ cvx_size_t cvx_scaling_elem(cvx_matrix_t *A, const cvx_scaling_t *W, cvx_mset_en
         }
     case CVXWS_V:
         if (ind < W->vcount) {
-            cvxm_map_data(A, W->dims->qdims[ind], 1, &W->data[W->indv[ind]]);
-            return W->indv[ind];
+            cvx_size_t n = W->indv[ind+1] - W->indv[ind];
+            cvxm_map_data(A, n, 1, &W->data[W->indv[ind]]);
+            return n;
         }
     case CVXWS_R:
         if (ind < W->rcount) {
-            cvxm_map_data(A, W->dims->sdims[ind], W->dims->sdims[ind], &W->data[W->indr[ind]]);
-            return W->dims->sdims[ind];
+            cvx_size_t n = W->indr[2*ind+1];
+            cvxm_map_data(A, n, n, &W->data[W->indr[2*ind]]);
+            return n;
         }
     case CVXWS_RTI:
         if (ind < W->rcount) {
-            cvxm_map_data(A, W->dims->sdims[ind], W->dims->sdims[ind], &W->data[W->indrti[ind]]);
-            return W->dims->sdims[ind];
+            cvx_size_t n = W->indrti[2*ind+1];
+            cvxm_map_data(A, n, n, &W->data[W->indrti[2*ind]]);
+            return n;
         }
     }
     return 0;
