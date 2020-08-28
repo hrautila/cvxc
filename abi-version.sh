c45c5ebdb39ac63650193c37d5df69f4f3336a2a
Harri Rautila <harri.rautila@gmail.com>
2020-08-29 00:16:10 +0300

    Add autoconf configuration.

---
+++ abi-version.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+CMDDIR=$(dirname $0)
+
+CONVEX_H="${CMDDIR}/src/include/convex.h"
+if [ ! -f "$CONVEX_H" ]; then
+    echo "abi-version.sh: header convex.h not available"
+    exit 1
+fi
+
+CURRENT=$(awk '/^#define +CVXC_ABI_CURRENT/ {print $3}' $CONVEX_H)
+REVISION=$(awk '/^#define +CVXC_ABI_REVISION/ {print $3}' $CONVEX_H)
+AGE=$(awk '/^#define +CVXC_ABI_AGE/ {print $3}' $CONVEX_H)
+
+if [ -z "$CURRENT" -o -z "$REVISION" -o -z "$AGE" ]; then
+    echo "abi-version.sh: abi version macros not found."
+    exit 1
+fi
+
+case $1 in
+    -libtool)
+        printf '%s' "$CURRENT:$REVISION:$AGE"
+        ;;
+    *)
+        printf '%s' "$CURRENT.$REVISION.$AGE"
+        ;;
+esac
+
