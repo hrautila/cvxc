3e5354da2917d8df1dd2a0032adac0766d671267
Harri Rautila <harri.rautila@gmail.com>
2020-09-01 01:17:07 +0300

    Print solution helper.

---
+++ tests/helper.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+#include "cvxc.h"
+
+static char *solution_name[] = {
+    "Optimal",
+    "Unknown",
+    "Primal Infeasible",
+    "Dual Infeasible",
+    "Singular"
+};
+
+int print_solution(cvxc_solution_t *sol)
+{
+    printf("status      : %2d [%s]\n", sol->status, solution_name[sol->status]);
+    printf("primal obj  : %13.6e\n", sol->primal_objective);
+    printf("dual obj    : %13.6e\n", sol->dual_objective);
+    printf("primal inf  : %13.6e\n", sol->primal_infeasibility);
+    printf("dual int    : %13.6e\n", sol->dual_infeasibility);
+    printf("primal slack: %13.6e\n", sol->primal_slack);
+    printf("dual slack  : %13.6e\n", sol->dual_slack);
+    printf("primal cert : %13.6e\n", sol->primal_residual_cert);
+    printf("dual cert   : %13.6e\n", sol->dual_residual_cert);
+    printf("gap         : %13.6e\n", sol->gap);
+    printf("relative gap: %13.6e\n", sol->relative_gap);
+    printf("iterations  : %d\n", sol->iterations);
+    if (sol->status != CVXC_STAT_OPTIMAL)
+        return -1;
+    cvxc_mat_printf(stdout, "%13.6e", sol->x, "x");
+    cvxc_mat_printf(stdout, "%13.6e", sol->s, "s");
+    cvxc_mat_printf(stdout, "%13.6e", sol->y, "y");
+    cvxc_mat_printf(stdout, "%13.6e", sol->z, "z");
+    return 0;
+}
