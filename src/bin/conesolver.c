798672c49215f9d8a2491c43c72c274a408a6492
Harri Rautila <harri.rautila@gmail.com>
2020-12-04 18:08:42 +0200

    Working conelp solver.

--- src/bin/conesolver.c
+++ src/bin/conesolver.c
@@ -7,7 +7,7 @@
  */
 #include <getopt.h>
 #include <string.h>
-
+#include <stdio.h>
 #include "cvxc.h"
 
 enum long_opt_enum {
@@ -17,38 +17,61 @@ enum long_opt_enum {
 
 
 static struct option long_options[] = {
-#if 0
-    {"matrixG",   required_argument, 0, 'G'},
-    {"matrixA",   required_argument, 0, 'A'},
-    {"matrixb",   required_argument, 0, 'b'},
-    {"matrixc",   required_argument, 0, 'c'},
-    {"matrixh",   required_argument, 0, 'h'},
-#endif
-    {"mm-mask",   required_argument, 0, 'M'},
-    {"max-iter",  required_argument, 0, 'I'},
-    {"linear",    required_argument, 0, 'L'},
-    {"socp",      required_argument, 0, 'Q'},
-    {"sdp" ,      required_argument, 0, 'S'},
-    {"json",      required_argument, 0, 'J'},
+    {"matrix-G",   required_argument, 0, 'G'},
+    {"matrix-A",   required_argument, 0, 'A'},
+    {"matrix-b",   required_argument, 0, 'b'},
+    {"matrix-c",   required_argument, 0, 'c'},
+    {"matrix-h",   required_argument, 0, 'h'},
+    {"dims",       required_argument, 0, 'd'},
+    {"program",    required_argument, 0, 'P'},
+    {"mm-mask",    required_argument, 0, 'm'},
+    {"max-iter",   required_argument, 0, 'i'},
+    {"linear",     required_argument, 0, 'L'},
+    {"socp",       required_argument, 0, 'Q'},
+    {"sdp" ,       required_argument, 0, 'S'},
+    {"json",       no_argument,       0, 'j'},
+    {"verbose",    no_argument,       0, 'v'},
+    {"refinement", required_argument, 0, 'r'},
+    {0,            0,                 0, 0}
 };
 
-//#define SHORT_OPTS "A:G:c:h:b:L:Q:S:"
-#define SHORT_OPTS "L:Q:S:M:I:"
+#define SHORT_OPTS "A:G:c:h:b:d:P:L:Q:S:m:i:jv"
 
-int *parse_intlist(int *n, char *arg)
+struct solver_args {
+    char *name_G;
+    char *name_h;
+    char *name_c;
+    char *name_A;
+    char *name_b;
+    char *name_dims;
+    char *name_prob;
+    char *name_cp;
+    cvxc_size_t L;
+    cvxc_size_t nL;
+    cvxc_size_t nQ;
+    cvxc_size_t nS;
+    cvxc_size_t *Q;
+    cvxc_size_t *S;
+    cvxc_size_t maxiter;
+    int verbose;
+    int refinement;
+    int json;
+};
+
+cvxc_size_t *parse_intlist(cvxc_size_t *n, char *arg)
 {
     char *tok, *cp;
     int k, count = 0;
     if (strlen(arg) == 0)
-        return (int *)0;
+        return (cvxc_size_t *)0;
 
     for (cp = strchr(arg, ','); cp; cp = strchr(cp+1, ',')) {
         count += 1;
     }
     // we have count+1 values, need count+2 space to zero fill last entry
-    int *p = (int *)calloc(count+2, sizeof(int));
+    cvxc_size_t *p = (cvxc_size_t *)calloc(count+2, sizeof(cvxc_size_t));
     if (!p)
-        return (int *)0;
+        return p;;
     if (n)
         *n = count + 1;
 
@@ -67,120 +90,251 @@ int *parse_intlist(int *n, char *arg)
     return p;
 }
 
-// read matrix market formatted file
-int mmread_file(cvxc_matrix_t *A, const char *path)
+/*
+ * read matrix market formatted file
+ */
+int solver_read_matrix(cvxc_matrix_t **A, const char *path)
 {
-    cvxm_init(A, 0, 0);
+    int err;
+    cvxc_stream_t ios;
+    if (*A) {
+        cvxm_init(*A, 0, 0);
+    }
     if (!path) {
         return 0;
     }
+    int json = strstr(path, ".json") != 0;
+
     FILE *fp = fopen(path, "r");
     if (!fp) {
         perror(path);
         return -1;
     }
-    int err = cvxm_mmload(A, fp);
+    cvxc_file_stream(&ios, fp);
+    if (json) {
+        err = cvxc_json_matrix_read(A, &ios);
+    } else {
+        err = 0; // cvxm_mmload(A, fp);
+    }
     fclose(fp);
     return err;
 }
 
-#define MM_MASK_DEFAULT "matrix_%c.mtx"
-
-void print_solution(cvxc_solution_t *sol)
+int solver_read_params(cvxc_params_t **pptr, const char *path)
 {
-    printf("status      : %2d [%s]\n", sol->status, solution_name[sol->status]);
-    printf("primal obj  : %13.6e\n", sol->primal_objective);
-    printf("dual obj    : %13.6e\n", sol->dual_objective);
-    printf("primal inf  : %13.6e\n", sol->primal_infeasibility);
-    printf("dual int    : %13.6e\n", sol->dual_infeasibility);
-    printf("primal slack: %13.6e\n", sol->primal_slack);
-    printf("dual slack  : %13.6e\n", sol->dual_slack);
-    printf("primal cert : %13.6e\n", sol->primal_residual_cert);
-    printf("dual cert   : %13.6e\n", sol->dual_residual_cert);
-    printf("gap         : %13.6e\n", sol->gap);
-    printf("relative gap: %13.6e\n", sol->relative_gap);
-    printf("iterations  : %d\n", sol->iterations);
-    if (sol->status != CVXC_STAT_OPTIMAL) 
-        return;
-    cvxc_mat_printf(stdout, "%13.6e", sol->x, "x");
-    cvxc_mat_printf(stdout, "%13.6e", sol->s, "s");
-    cvxc_mat_printf(stdout, "%13.6e", sol->y, "y");
-    cvxc_mat_printf(stdout, "%13.6e", sol->z, "z");
+    FILE *fp;
+    cvxc_stream_t ios;
+
+    if (!path || *path == '-') {
+        fp = stdin;
+    } else {
+        fp = fopen(path, "r");
+        if (!fp) {
+            perror(path);
+            return -1;
+        }
+    }
+    cvxc_file_stream(&ios, fp);
+    int err = cvxc_json_read_params(pptr, &ios);
+    if (fp != stdin)
+        fclose(fp);
+    return err;
 }
 
+int solver_write_solution(const char *path, const cvxc_solution_t *sol)
+{
+    FILE *fp;
+    cvxc_stream_t ios;
+
+    if (!path || *path == '-') {
+        fp = stdout;
+    } else {
+        fp = fopen(path, "w");
+        if (!fp) {
+            perror(path);
+            return -1;
+        }
+    }
+    cvxc_file_stream(&ios, fp);
+    int err = cvxc_json_write_solution(&ios, sol);
+    if (fp != stdout)
+        fclose(fp);
+    return err;
+}
+
+int solver_init_dims(cvxc_dimset_t **dims, struct solver_args *args, cvxc_size_t hrows)
+{
+    if (args->name_dims) {
+        cvxc_stream_t ios;
+        FILE *fp = fopen(args->name_dims, "r");
+        if (fp) {
+            cvxc_file_stream(&ios, fp);
+        } else {
+            cvxc_str_stream(&ios, args->name_dims, strlen(args->name_dims));
+        }
+        int err = cvxc_json_dimset_read(dims, &ios);
+        if (fp)
+            fclose(fp);
+        return err;
+    }
+    if (!*dims) {
+        *dims = cvxc_dimset_new(0, 0, 0, 0);
+    }
+    if (args->L + args->nL + args->nQ + args->nS > 0) {
+        if (!cvxc_dimset_alloc(*dims, args->L, args->Q, args->S))
+            return -1;
+        if (args->nL > 0)
+            (*dims)->mnl = args->nL;
+    } else {
+        /* No dimensions defined, assume linear with hrows contraints. */
+        cvxc_dimset_create(*dims, 0, hrows, 0, 0);
+    }
+    return 0;
+}
+
+void print_params(cvxc_params_t *p)
+{
+    cvxc_stream_t ios;
+    cvxc_file_stream(&ios, stdout);
+    cvxc_json_write_params(&ios, p->opts, p->dims,
+                           p->c, p->G, p->h, p->A, p->b);
+}
+
+static
+int solver_have_file_args(struct solver_args *args)
+{
+    return args->name_G
+        || args->name_h
+        || args->name_c
+        || args->name_A
+        || args->name_b
+        || args->name_prob;
+}
+
+int solve_conelp(struct solver_args *args)
+{
+    cvxc_solopts_t opts = {0};
+    cvxc_params_t *pptr, params = {0};
+    pptr = &params;
+
+    opts.refinement = args->refinement;
+    opts.show_progress = args->verbose;
+    opts.max_iter = args->maxiter;
+
+    if (!solver_have_file_args(args) || args->name_prob) {
+        if (solver_read_params(&pptr, args->name_prob) < 0)
+            return -1;
+    }
+    if (solver_read_matrix(&params.G, args->name_G) < 0)
+        return -1;
+    if (solver_read_matrix(&params.h, args->name_h) < 0)
+        return -1;
+    if (solver_read_matrix(&params.c, args->name_c) < 0)
+        return -1;
+    if (solver_read_matrix(&params.A, args->name_A) < 0)
+        return -1;
+    if (solver_read_matrix(&params.b, args->name_b) < 0)
+        return -1;
+
+    cvxc_size_t rows, cols;
+    cvxm_size(&rows, &cols, params.h);
+    if (solver_init_dims(&params.dims, args, rows) < 0)
+        return -1;
+
+    if (!params.G || !params.h || !params.c) {
+        fprintf(stderr, "matrices c, G, h need to defined\n");
+        return -1;
+    }
+    if ((!params.A && params.b) || (params.A && !params.b)) {
+        fprintf(stderr, "must have both  A and b\n");
+        return -1;
+    }
+
+    if (!params.A && !params.b) {
+        cvxc_size_t rows, cols;
+        cvxm_size(&rows, &cols, params.G);
+        params.A = cvxm_new(0, cols);
+        params.b = cvxm_new(0, 1);
+    }
+    // print_params(&params);
+
+    cvxc_problem_t cp;
+    cvxc_size_t nbytes =
+        cvxc_conelp_setup(&cp, params.c, params.G, params.h,
+                          params.A, params.b, params.dims, (cvxc_kktsolver_t *)0);
+    if (nbytes == 0) {
+        fprintf(stderr, "Conelp problem setup failed\n");
+        return -1;
+    }
+    cvxc_conelp_compute_start(&cp);
+    cvxc_conelp_solve(&cp, params.opts ? params.opts : &opts);
+
+    solver_write_solution("-", &cp.solution);
+
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
-    char *mm_mask = MM_MASK_DEFAULT;
-    char name[512];
-
-    int *dim_Q = (int *)0;
-    int *dim_S = (int *)0;
-    int nQ, nS;
     int opt;
-    int dim_L;
     int option_index;
-    int max_iter = 100;
 
-    cvxc_matrix_t A, G, c, b, h;
-    cvxc_dimset_t dims;
+    struct solver_args args = {0};
 
     while ((opt = getopt_long(argc, argv, SHORT_OPTS, long_options, &option_index)) != -1) {
         switch (opt) {
-        case 'M':
-            mm_mask = optarg;
+        case 'A':
+            if (optarg)
+                args.name_A = optarg;
             break;
-        case 'I':
-            max_iter = atoi(optarg);
+        case 'G':
+            if (optarg)
+                args.name_G = optarg;
+            break;
+        case 'h':
+            if (optarg)
+                args.name_h = optarg;
+            break;
+        case 'b':
+            if (optarg)
+                args.name_b = optarg;
+            break;
+        case 'c':
+            if (optarg)
+                args.name_c = optarg;
+            break;
+        case 'd':
+            if (optarg)
+                args.name_dims = optarg;
+        case 'm':
+            // mm_mask = optarg;
+            break;
+        case 'i':
+            args.maxiter = atoi(optarg);
             break;
         case 'L':
-            dim_L = atoi(optarg);
+            args.L = atoi(optarg);
             break;
         case 'Q':
-            dim_Q = parse_intlist(&nQ, optarg);
+            args.Q = parse_intlist(&args.nQ, optarg);
             break;
         case 'S':
-            dim_S = parse_intlist(&nS, optarg);
+            args.S = parse_intlist(&args.nS, optarg);
             break;
+        case 'j':
+            args.json = 1;
+            break;
+        case 'r':
+            args.refinement = atoi(optarg);
+            break;
+        case 'v':
+            args.verbose++;
+            break;
+        default:
+            fprintf(stderr, "unknown option: %c\n", opt);
+            exit(1);
         }
     }
-
-    cvxc_dimset_alloc(&dims, dim_L, dim_Q, dim_S);
-
-    snprintf(name, sizeof(name), mm_mask, 'c');
-    mmread_file(&c, name);
-
-    snprintf(name, sizeof(name), mm_mask, 'G');
-    mmread_file(&G, name);
-
-    snprintf(name, sizeof(name), mm_mask, 'h');
-    mmread_file(&h, name);
-
-    snprintf(name, sizeof(name), mm_mask, 'A');
-    mmread_file(&A, name);
-
-    snprintf(name, sizeof(name), mm_mask, 'b');
-    mmread_file(&b, name);
-
-    cvxc_conelp_problem_t cp;
-    cvxc_solopts_t opts = (cvxc_solopts_t){
-        .abstol = 0.0,
-        .reltol = 0.0,
-        .feastol = 0.0,
-        .max_iter = max_iter,
-        .debug = 0,
-        .refinement = 0,
-        .kkt_solver_name = 0,
-        .show_progress = 1
-    };
-
-    cvxc_size_t nbytes =
-        cvxc_conelp_setup(&cp, &c, &G, &h, &A, &b, &dims, (cvxc_kktsolver_t *)0);
-    if (nbytes == 0) {
-        fprintf(stderr, "Conelp problem setup failed\n");
-        exit(1);
-    }
-    cvxc_conelp_compute_start(&cp);
-    cvxc_conelp_solve(&cp, &opts);
-    print_solution(&cp.solution);
+    int err = solve_conelp(&args);
+    return err;
 }
