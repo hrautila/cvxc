2a6cdde643a10f53edd4ee4cea9b1eebcb73f633
Harri Rautila <harri.rautila@gmail.com>
2021-04-22 23:55:50 +0300

    Inline helpers changed to static inline.

--- src/lib/kkt.c
+++ src/lib/kkt.c
@@ -19,15 +19,5 @@
 
 #if __STDC_VERSION__
 // inline function reference
-void cvxc_ldlsolver_init(cvxc_kktsolver_t *S,
-                        cvxc_problem_t *cp, int n, int m, const cvxc_dimset_t *dims);
-
-int cvxc_kktinit(cvxc_kktsolver_t *S,
-                cvxc_problem_t *cp, int n, int m, const cvxc_dimset_t *dims);
-
-int cvxc_kktfactor(cvxc_kktsolver_t *S,
-                  cvxc_scaling_t *W, cvxc_matrix_t *H, cvxc_matrix_t *Df);
-int cvxc_kktsolve(cvxc_kktsolver_t *S, cvxc_matrix_t *x, cvxc_matrix_t *y, cvxc_matgrp_t *z_g);
-void cvxc_kktrelease(cvxc_kktsolver_t *kkt);
 
 #endif
